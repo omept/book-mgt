3f5a3a94e9e708911b97dc55764ab86f2fd2f40d
George Onwuasoanya <georgetheprogrammer@gmail.com>
2021-10-23 11:26:07 +0100

    app mac build added

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -8,21 +8,29 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/ong-gtp/book-mgt/pkg/errors"
 	"github.com/ong-gtp/book-mgt/pkg/models"
+	"github.com/ong-gtp/book-mgt/pkg/utils"
 )
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
+	book := models.Book{}
+	utils.ParseBody(r, &book)
+	b, err := book.CreateBook()
+	errors.DBErrorCheck(err)
 
+	res, err2 := json.Marshal(b)
+	errors.ErrorCheck(err2)
+
+	utils.Ok(res, w)
 }
 
 func GetBook(w http.ResponseWriter, r *http.Request) {
 	newBook, err := models.GetAllBooks()
-	errors.ErrorCheck(err)
+	errors.DBErrorCheck(err)
 
-	res, err := json.Marshal(newBook)
-	errors.ErrorCheck(err)
+	res, er2 := json.Marshal(newBook)
+	errors.ErrorCheck(er2)
 
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
+	utils.Ok(res, w)
 }
 
 func GetBookById(w http.ResponseWriter, r *http.Request) {
@@ -34,12 +42,56 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 
 	book, dbInst := models.GetBookById(id)
 	errors.DBErrorCheck(dbInst)
-}
 
-func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	res, er2 := json.Marshal(book)
+	errors.ErrorCheck(er2)
 
+	utils.Ok(res, w)
 }
 
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
 
+	id, err := strconv.ParseInt(bookId, 0, 0)
+	errors.ErrorCheck(err)
+
+	_, dbInst := models.DeleteBook(id)
+	errors.DBErrorCheck(dbInst)
+
+	utils.OkEmpty("Deleted successfully", w)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+
+	id, err := strconv.ParseInt(bookId, 0, 0)
+	errors.ErrorCheck(err)
+
+	// get book from database
+	bk, dbInst := models.GetBookById(id)
+	errors.DBErrorCheck(dbInst)
+	if bk.ID == 0 {
+		utils.OkEmpty("Invalid book", w)
+		return
+	}
+
+	// update book with json from route body
+	book := models.Book{} // the response body should conform with book type
+	// decode response body into book
+	decoder := json.NewDecoder(r.Body)
+	err3 := decoder.Decode(&book)
+	errors.ErrorCheck(err3)
+	// update bk with request
+	bk.Name = book.Name
+	bk.Author = book.Author
+	bk.Publication = book.Publication
+	newBk, err2 := bk.UpdateBook()
+	errors.DBErrorCheck(err2)
+	// prepare updated data for response
+	res, err4 := json.Marshal(newBk)
+	errors.ErrorCheck(err4)
+
+	utils.Ok(res, w)
 }
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -4,7 +4,13 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/ong-gtp/book-mgt/pkg/errors"
 )
+
+type emptyOk struct {
+	Message string
+}
 
 func ParseBody(r *http.Request, x interface{}) {
 	if body, err := ioutil.ReadAll(r.Body); err == nil {
@@ -12,4 +18,16 @@ func ParseBody(r *http.Request, x interface{}) {
 			return
 		}
 	}
+}
+
+func Ok(res []byte, w http.ResponseWriter) {
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func OkEmpty(message string, w http.ResponseWriter) {
+	m := emptyOk{message}
+	res, err := json.Marshal(m)
+	errors.ErrorCheck(err)
+	Ok(res, w)
 }
